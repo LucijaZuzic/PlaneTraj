68be1cd2ef4b84c389c496c6d418865d6d4d1b78
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-01-10 18:01:59 +0100

    Ungz weather

---
+++ process_weather.py
@@ -0,0 +1,41 @@
+import requests 
+from datetime import datetime, timedelta
+import os
+import shutil
+import requests
+import gzip
+
+def download_weather(airport, datetime_obj):
+ 
+
+    end_url = ".en.utf8.00000000.csv.gz"
+     
+    date_str = datetime_obj.strftime("%d.%m.%Y")
+    month_str = datetime_obj.strftime("%m")
+    if month_str[0] == "0":
+        month_str = month_str[1:]
+    day_str = datetime_obj.strftime("%d")
+    if day_str[0] == "0":
+        day_str = day_str[1:]
+
+    start_of_filename = airport + "." + date_str + "." + date_str + ".1.0.0" 
+
+    filename_to_save = start_of_filename + end_url 
+    dir_to_save = "rp5/" + airport
+
+    print(dir_to_save + "/" + filename_to_save)
+
+    if os.path.isfile(dir_to_save + "/" + filename_to_save):
+  
+        with gzip.open(dir_to_save + "/" + filename_to_save, 'rb') as f_in:
+            with open((dir_to_save + "/" + filename_to_save).replace(".gz", ""), 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+     
+dt = datetime(day = 25, month = 5, year = 2020)
+dt_end = datetime(day = 27, month = 6, year = 2022) 
+
+epoch_time = datetime(1970, 1, 1)
+
+while dt <= dt_end:
+    download_weather("YMML", dt)
+    dt = dt + timedelta(days = 7)
