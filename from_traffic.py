a5802dc5ca5fc0a78422b13d839a641811bfad0d
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-01-12 07:08:42 +0100

    Remove invalid traj

--- from_traffic.py
+++ from_traffic.py
@@ -1,16 +1,25 @@
-import traffic.core 
+from traffic.core import time, Traffic, Flight
 from rich.pretty import pprint
 from rich.console import Console
-from datetime import datetime, timedelta
+from datetime import datetime
 import pandas as pd
 import numpy as np
 import os 
+from traffic.data import airports
+import matplotlib.pyplot as plt
+from cartes.crs import Mercator, Lambert93, EPSG_3112
+
+#from cartes.crs import valid_crs
+#print(valid_crs("Sydney, Australia")["auth_name"])
+#print(valid_crs("Sydney, Australia")["code"])
 
 console = Console() 
  
 epoch_time = datetime(1970, 1, 1)
  
+num_trajs = 0
 to_runway = dict()
+flights_all = []
 
 for usable_traj_filename in os.listdir("usable_trajs"):
     if ".csv" not in usable_traj_filename:
@@ -29,7 +38,7 @@ for usable_traj_filename in os.listdir("usable_trajs"):
 
     timestamps_array = []
     for ts in pd_file["time"]: 
-        timestamps_array.append(traffic.core.time.to_datetime(ts))  
+        timestamps_array.append(time.to_datetime(ts))  
 
     altitudes_array = []
     for ba in pd_file["baroaltitude"]:
@@ -49,14 +58,16 @@ for usable_traj_filename in os.listdir("usable_trajs"):
   
     pd_file_new.to_pickle("usable_trajs/" + usable_traj_filename.replace(".csv", ".pkl")) 
 
-    f = traffic.core.Flight.from_file("usable_trajs/" + usable_traj_filename.replace(".csv", ".pkl"))
+    f = Flight.from_file("usable_trajs/" + usable_traj_filename.replace(".csv", ".pkl"))
     
     #print(f)
     #pprint(f)
     #console.print(f)
     #print(f.start, f.callsign, f.icao24, f.stop, f.squawk, f.distance())
     ta = f.takeoff_airport() 
-    #print(ta)
+    print(ta)
+
+    fnd = False
     
     for el in f.takeoff_from_runway(ta):
         print(el)
@@ -64,8 +75,32 @@ for usable_traj_filename in os.listdir("usable_trajs"):
         pd_el = pd.read_csv("mynew.csv")
         runway_nums = list(pd_el["runway"])
         print(runway_nums)
-        os.remove("mynew.csv")
+        #os.remove("mynew.csv")
+        fnd = True
 
+    num_trajs += 1
+
+    flights_all.append(f)
+    
+    f = f.compute_xy(projection = Lambert93()) 
+    f.to_csv("mynew.csv")
+    pd_el = pd.read_csv("mynew.csv")
+    xv = list(pd_el["x"])
+    yv = list(pd_el["y"])
+    xv = [(x - xv[0]) / 1000 for x in xv[:3]]
+    yv = [(y - yv[0]) / 1000  for y in yv[:3]]
+
+    print(xv, yv) 
+    fig, ax = plt.subplots(subplot_kw = dict(projection = EPSG_3112()))
+    f.plot(ax)
+    #plt.savefig("myfile.png", bbox_inches = "tight") 
+    print(xv, yv) 
+
+    if num_trajs == 10:
+        break
+
+    break
+ 
 # https://opensky-network.org/data/data-tools#d1
 # https://easychair.org/publications/paper/BXjT
 # https://traffic-viz.github.io/index.html
